163310fa97984c1b7ac8b6411d3a3ae1bff39e05
Alejandro Mangione <alejandro.ferraiolo@brasilct.com>
2016-10-06 14:01:11 -0300

    add basic authetication just in home controller

--- app/controllers/duckrails/home_controller.rb
+++ app/controllers/duckrails/home_controller.rb
@@ -1,5 +1,6 @@
 module Duckrails
   class HomeController < ApplicationController
+    http_basic_authenticate_with name: ENV['AUTH_USER'], password: ENV['AUTH_PASSWORD'] if Rails.env != 'development'
     def index
     end
   end
